942a2a26b779c5fd8171a1048296527a63d44365
Caleb Choi <calebc1205@gmail.com>
2018-05-11 01:01:22 -0700

    set up file structure for components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Search from './containers/Search.jsx';
+import TweetList from './presentational/TweetList.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      tweets: [],
+      sortBy: 'favorites',
+    };
+  }
+
+  search = () => {
+    
   }
 
   render() {
     return (
       <div className="app-container">
-        Twitter Content Lab
+        <Search />
+        <TweetList tweets={this.state.tweets} sortBy={this.state.sortBy} />
       </div>
     )
   }
