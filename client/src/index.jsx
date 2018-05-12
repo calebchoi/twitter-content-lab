34bb335a09ce0ae4a2c72c253c7900f76314039c
Caleb Choi <calebc1205@gmail.com>
2018-05-11 21:51:06 -0700

    added website heading and tweetList component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Search from './containers/Search.jsx';
-import TweetList from './presentational/TweetList.jsx';
+import TweetList from './containers/TweetList.jsx';
+import testData from '../../server/testData.js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      tweets: [],
-      sortBy: 'favorites',
+      tweets: testData.tweets,
     };
   }
 
-  search = () => {
-    
+  searchTweets = () => {
+
   }
 
   render() {
     return (
       <div className="app-container">
-        <Search />
-        <TweetList tweets={this.state.tweets} sortBy={this.state.sortBy} />
+        <h1>Twitter Content Lab</h1>
+        <Search searchTweets={this.searchTweets} />
+        <TweetList tweets={this.state.tweets} />
       </div>
     )
   }
