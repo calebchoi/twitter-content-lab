f4c92c21bc9a85bbf07031e1bd826d4480e6a568
Caleb Choi <calebc1205@gmail.com>
2018-05-11 21:49:52 -0700

    formatted tweet with basic information

---
+++ client/src/presentational/Tweet.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Tweet = props => (
+  <div className="tweet">
+    <h5>
+      {props.tweet.user.name}&emsp;
+      <small className="text-muted">@{props.tweet.user.screen_name}</small>&emsp;
+      <small className="text-muted">{props.tweet.created_at.split(' ').slice(0,4).join(' ')}</small>
+    </h5>
+    <p>{props.tweet.text}</p>
+    <p>Favorited: {props.tweet.favorite_count} &emsp; Retweeted: {props.tweet.retweet_count}</p>
+  </div>
+);
+
+export default Tweet;
